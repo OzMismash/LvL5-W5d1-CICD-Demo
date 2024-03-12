a6f9799f379561863d9674536cee8a9be3946ad1
Oscar Mismash <oscarfmismash@gmail.com>
2024-03-12 18:00:31 +1300

    added stuffs

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+const fibonacci = (upto) => {
+    if (isNaN(upto)) {
+        return "Not a Number!";
+    }
+    if (upto == 0) {
+        return [];
+    }
+    if (upto == 1) {
+        return [0]
+    } else if (upto == 2) {
+        return [0, 1];
+    } else {
+        return "Not implemented!";
+    }
+}
+
+module.exports = fibonacci
